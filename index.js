97f00431a251d550c979ce0ee3be7867479646e8
sinplay <sin_play@outlook.com>
2017-05-01 11:18:09 +0700

    เพิ่ม firebase

--- index.js
+++ index.js
@@ -17,3 +17,23 @@ document.getElementById("reportInfo").onclick = () => {
     app.add(document.getElementById("r").value, document.getElementById("reportForm").elements["status"].value)
     document.getElementById("reportForm").reset()
 }
+var config = {
+    apiKey: "AIzaSyAFV4VAHm0Xa1EJt48xzVldwv3xv_f6kww",
+    authDomain: "cm-traffic.firebaseapp.com",
+    databaseURL: "https://cm-traffic.firebaseio.com",
+    projectId: "cm-traffic",
+    storageBucket: "cm-traffic.appspot.com",
+    messagingSenderId: "621929469587"
+};
+var convertFromFirebase = (data) => {
+  var ids = Object.keys(data)
+  return ids.map(id => data[id])
+}
+firebase.initializeApp(config);
+var database = firebase.database();
+var trafficDatabase = database.ref("/")
+trafficDatabase.on("value", (trafficBlocks) => {
+    console.log(trafficBlocks.val())
+    var data =convertFromFirebase(trafficBlocks.val())
+    app.trafficBlocks=data
+})
